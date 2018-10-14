195b7f55e598e4f378bf3e5a90a3a624f49b9d4c
Ceren <cerenaltay.lsb@gmail.com>
2018-10-14 12:58:34 +0100

    Authorisation 2-> indirect references

--- SecureWebsitePractices2/SecureWebsitePractices2/Controllers/ProfileController.cs
+++ SecureWebsitePractices2/SecureWebsitePractices2/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.ApplicationInsights.Extensibility.Implementation;
 using SecureWebsitePractices2.Models;
+using SecureWebsitePractices2.ReferenceMaps;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,26 +10,65 @@ using System.Web.Profile;
 using UserContext = SecureWebsitePractices2.Models.UserContext;
 
 namespace SecureWebsitePractices2.Controllers
-{
+{    
     public class ProfileController : Controller
     {
-        // GET: Profile
-        public ActionResult Index(string userName)
+        //original
+        /*
+          // GET: Profile
+          public ActionResult Index(string userName)
+          {
+              if (!User.Identity.IsAuthenticated)
+              {
+                  throw new ApplicationException("User not authenticated");
+              }
+
+              //Access control
+              //if (User.Identity.Name != userName)
+              //{
+              //    throw new ApplicationException("Access not authorised.");
+              //}
+
+
+              ProfileModel profile = new ProfileModel();
+
+              using (UserContext context = new UserContext())
+              {
+                  profile = context.Profiles.SingleOrDefault(x => x.UserName == userName);
+              }
+
+              if (profile == null)
+              {
+                  throw new ApplicationException("Profile does not exist");
+              }
+
+              return Json(new
+              {
+                  UserName = profile.UserName,
+                  Address = profile.Address,
+                  BirthDate = profile.BirthDate.ToString("dd MM yyyy"),
+                  NINumber = profile.NINumber
+              },
+                JsonRequestBehavior.AllowGet);
+          }
+      */
+
+        //Authorisation 2-> indirect ref
+        public ActionResult Index(string id)
         {
             if (!User.Identity.IsAuthenticated)
             {
                 throw new ApplicationException("User not authenticated");
             }
 
-            //Access control
-            //if (User.Identity.Name != userName)
-            //{
-            //    throw new ApplicationException("Access not authorised.");
-            //}
+            var userName = id.GetDirectReference();
 
+            if (User.Identity.Name != userName)
+            {
+                throw new ApplicationException("User not authorised");
+            }
 
             ProfileModel profile = new ProfileModel();
-
             using (UserContext context = new UserContext())
             {
                 profile = context.Profiles.SingleOrDefault(x => x.UserName == userName);
@@ -48,5 +88,6 @@ namespace SecureWebsitePractices2.Controllers
             },
               JsonRequestBehavior.AllowGet);
         }
+
     }
 }
---
+++ SecureWebsitePractices2/SecureWebsitePractices2/ReferenceMaps/IndirectReferenceMap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace SecureWebsitePractices2.ReferenceMaps
+{
+    public static class IndirectReferenceMap
+    {
+        public static string GetIndirectReference(this string directRef)
+        {
+            var map = (Dictionary<string, string>)HttpContext.Current.Session["Map"];
+            return map == null ? AddDirectReference(directRef) : map[directRef];
+        }
+
+        public static string GetDirectReference(this string indirectRef)
+        {
+            var map = HttpContext.Current.Session["Map"];
+
+            if (map == null)
+            {
+                throw new ApplicationException("No map found");
+            }
+
+            return ((Dictionary<string, string>)map)[indirectRef];
+        }
+
+        private static string AddDirectReference(string directRef)
+        {
+            var rng = new RNGCryptoServiceProvider();
+            var buff = new byte[32];
+            rng.GetBytes(buff);
+
+            var indirectRef = HttpServerUtility.UrlTokenEncode(buff);
+
+            var map = new Dictionary<string, string>
+        {
+          {directRef, indirectRef},
+          {indirectRef, directRef}
+        };
+
+            HttpContext.Current.Session["Map"] = map;
+            return indirectRef;
+        }
+    }
+}
